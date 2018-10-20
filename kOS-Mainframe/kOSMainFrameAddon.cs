18036806e823c64a99fb4470090cb58b5df3a91b
Bodo Junglas <junglas@objectcode.de>
2018-10-20 19:29:35 +0200

    Initial

---
+++ kOS-Mainframe/kOSMainFrameAddon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Safe.Exceptions;
+using kOS;
+using kOS.AddOns;
+using kOS.Suffixed;
+using kOS.Utilities;
+using UnityEngine;
+
+namespace kOSMainframe
+{
+	[kOSAddon("MainFrame")]
+	[kOS.Safe.Utilities.KOSNomenclature("MainFrameAddon")]
+	public class kOSMainFrameAddon : kOS.Suffixed.Addon
+    {
+		public kOSMainFrameAddon(SharedObjects shared) : base(shared)
+        {
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+			Debug.Log("MainFrame booting...");
+        }
+
+		public override BooleanValue Available()
+		{
+			return true;
+		}
+    }
+}
---
+++ kOS-Mainframe/kOSMainFramePlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace kOSMainframe
+{
+	[KSPAddon(KSPAddon.Startup.FlightEditorAndKSC, false)]
+	public class kOSMainFramePlugin : MonoBehaviour
+    {
+		public kOSMainFramePlugin()
+        {
+			Debug.Log("Mainframe started");
+        }
+
+		void Awake() {
+			Debug.Log("Mainframe woke up");
+		}
+
+		void Start() {
+			Debug.Log("Mainframe starts");
+		}
+    }
+}
