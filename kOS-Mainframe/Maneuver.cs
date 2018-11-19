e84fe57814480fbacb263af93599f48733786c66
Bodo Junglas <junglas@objectcode.de>
2018-11-19 19:31:30 +0100

    More maneuvers

--- kOS-Mainframe/Maneuver.cs
+++ kOS-Mainframe/Maneuver.cs
@@ -48,8 +48,15 @@ namespace kOSMainframe
 			AddSuffix("CHANGE_APOAPSIS", new TwoArgsSuffix<Node, ScalarValue, ScalarValue>(ChangeApoapsis, "Change apoapsis of given orbit (UT, newApR"));
 			AddSuffix("MATCH_PLANES", new OneArgsSuffix<Node, OrbitInfo>(MatchPlanes, "Match planes of given orbit with target orbit"));
 			AddSuffix("HOHMANN", new OneArgsSuffix<Node, OrbitInfo>(Hohmann, "Regular Hohmann transfer from given orbit to target orbit"));
+            AddSuffix("HOHMANN_LAMBERT", new TwoArgsSuffix<Node, OrbitInfo, ScalarValue>(HohmannLambert));
 			AddSuffix("BIIMPULSIVE", new OneArgsSuffix<Node, OrbitInfo>(BiImpulsive));
-		}
+            AddSuffix("CORRECTION", new OneArgsSuffix<Node, OrbitInfo>(CourseCorrection));
+            AddSuffix("CHEAPEST_CORRECTION", new OneArgsSuffix<Node, OrbitInfo>(CheapestCourseCorrection));
+            AddSuffix("CHEAPEST_CORRECTION_BODY", new TwoArgsSuffix<Node, BodyTarget, ScalarValue>(CheapestCourseCorrectionBody));
+            AddSuffix("CHEAPEST_CORRECTION_DIST", new TwoArgsSuffix<Node, OrbitInfo, ScalarValue>(CheapestCourseCorrectionDist));
+            AddSuffix("MATCH_VELOCITIES", new OneArgsSuffix<Node, OrbitInfo>(MatchVelocities));
+            AddSuffix("RETURN_FROM_MOON", new OneArgsSuffix<Node, ScalarValue>(ReturnFromMoon));
+        }
 
         private TimeSpan GetMinTime()
         {
@@ -121,7 +128,6 @@ namespace kOSMainframe
 
 		private Node Hohmann(OrbitInfo targetInfo)
 		{
-			var UT = Planetarium.GetUniversalTime();
             var target = GetOrbitFromOrbitInfo(targetInfo);
 			double burnUT = 0;
 			var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForHohmannTransfer(orbit, target, minUT, out burnUT);
@@ -129,9 +135,17 @@ namespace kOSMainframe
 			return NodeFromDeltaV(deltaV, burnUT);
 		}
 
-		private Node BiImpulsive(OrbitInfo targetInfo)
+        private Node HohmannLambert(OrbitInfo targetInfo, ScalarValue subtractProgradeDV)
+        {
+            var target = GetOrbitFromOrbitInfo(targetInfo);
+            double burnUT = 0;
+            var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForHohmannLambertTransfer(orbit, target, minUT, out burnUT, subtractProgradeDV);
+
+            return NodeFromDeltaV(deltaV, burnUT);
+        }
+
+        private Node BiImpulsive(OrbitInfo targetInfo)
 		{
-			var UT = Planetarium.GetUniversalTime();
             var target = GetOrbitFromOrbitInfo(targetInfo);
 			double burnUT = 0;
 			var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForBiImpulsiveAnnealed(orbit, target, minUT, out burnUT);
@@ -139,7 +153,59 @@ namespace kOSMainframe
             return NodeFromDeltaV(deltaV, burnUT);
 		}
 
-		private Node NodeFromDeltaV(Vector3d deltaV, double UT) {
+        private Node CourseCorrection(OrbitInfo targetInfo)
+        {
+            var target = GetOrbitFromOrbitInfo(targetInfo);
+            var deltaV = OrbitalManeuverCalculator.DeltaVForCourseCorrection(orbit, minUT, target);
+
+            return NodeFromDeltaV(deltaV, minUT);
+        }
+
+        private Node CheapestCourseCorrection(OrbitInfo targetInfo)
+        {
+            var target = GetOrbitFromOrbitInfo(targetInfo);
+            double burnUT = 0;
+            var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForCheapestCourseCorrection(orbit, minUT, target, out burnUT);
+
+            return NodeFromDeltaV(deltaV, burnUT);
+        }
+
+        private Node CheapestCourseCorrectionBody(BodyTarget body, ScalarValue finalPeR)
+        {
+            double burnUT = 0;
+            var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForCheapestCourseCorrection(orbit, minUT, body.Orbit, body.Body, finalPeR, out burnUT);
+
+            return NodeFromDeltaV(deltaV, burnUT);
+        }
+
+        private Node CheapestCourseCorrectionDist(OrbitInfo targetInfo, ScalarValue caDistance)
+        {
+            var target = GetOrbitFromOrbitInfo(targetInfo);
+            double burnUT = 0;
+            var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForCheapestCourseCorrection(orbit, minUT, target, caDistance, out burnUT);
+
+            return NodeFromDeltaV(deltaV, burnUT);
+        }
+
+        private Node MatchVelocities(OrbitInfo targetInfo)
+        {
+            var target = GetOrbitFromOrbitInfo(targetInfo);
+            double collisionUT = orbit.NextClosestApproachTime(target, minUT);
+
+            var deltaV = OrbitalManeuverCalculator.DeltaVToMatchVelocities(orbit, collisionUT, target);
+
+            return NodeFromDeltaV(deltaV, collisionUT);
+        }
+
+        private Node ReturnFromMoon(ScalarValue targetPrimaryRadius)
+        {
+            double burnUT = 0;
+            var deltaV = OrbitalManeuverCalculator.DeltaVAndTimeForMoonReturnEjection(orbit, minUT, targetPrimaryRadius, out burnUT);
+
+            return NodeFromDeltaV(deltaV, burnUT);
+        }
+
+        private Node NodeFromDeltaV(Vector3d deltaV, double UT) {
 			var nodeV = orbit.DeltaVToManeuverNodeCoordinates(UT, deltaV);
 
             return new Node(UT, nodeV.x, nodeV.y, nodeV.z, Shared);
