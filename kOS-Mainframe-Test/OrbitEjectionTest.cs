4ca56049b5c285dcaf6a091182a8e139ef3fd970
Bodo Junglas <junglas@objectcode.de>
2019-05-17 22:30:35 +0200

    Add desired orbit normal as parameter

--- kOS-Mainframe-Test/OrbitEjectionTest.cs
+++ kOS-Mainframe-Test/OrbitEjectionTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using kOSMainframe.Orbital;
+using System;
 
 namespace kOSMainframeTest {
     [TestFixture]
@@ -7,7 +8,15 @@ namespace kOSMainframeTest {
         [Test]
         public void TestInterplanetaryEjection() {
             var exitNode = OrbitIntercept.BiImpulsiveAnnealed(BodyTestRef.Kerbin.Orbit, BodyTestRef.Duna.Orbit, 0);
-            OrbitEjection.IdealEjection(BodyTestRef.Kerbin, exitNode.time, 80000 + BodyTestRef.Kerbin.radius, exitNode.deltaV);
+            var ejectionOrbit = OrbitEjection.IdealEjection(BodyTestRef.Kerbin, exitNode.time, 80000 + BodyTestRef.Kerbin.radius, BodyTestRef.Kerbin.orbit.SwappedOrbitNormal, exitNode.deltaV);
+            var ejectionP = ejectionOrbit.SwappedRelativePositionAtUT(exitNode.time);
+            var ejectionV = ejectionOrbit.SwappedOrbitalVelocityAtUT(exitNode.time);
+
+            Assert.True(Math.Abs(ejectionOrbit.Inclination) < 5, "Inclination");
+            Assert.AreEqual(exitNode.deltaV.x, ejectionV.x, 1e-5);
+            Assert.AreEqual(exitNode.deltaV.y, ejectionV.y, 1e-5);
+            Assert.AreEqual(exitNode.deltaV.z, ejectionV.z, 1e-5);
+            Assert.AreEqual(ejectionP.magnitude, BodyTestRef.Kerbin.SOIRadius, 1e-5);
         }
     }
 }
--- kOS-Mainframe/Orbital/OrbitEjection.cs
+++ kOS-Mainframe/Orbital/OrbitEjection.cs
@@ -6,16 +6,17 @@ namespace kOSMainframe.Orbital {
         /// <summary>
         /// Calculates the "ideal" ejection orbit for a given exit velocity at
         /// a given time. 
-        /// To get a unique result the periapsis of the ejection orbit has to be 
-        /// defined as well. As a rule of thumb: The periapsis should be greater
-        /// than the radius of the body (and its atmosphere), but the lower the
-        /// better.
+        /// To get a unique result the periapsis of the ejection orbit and a
+        /// desired orbit normal has to be defined as well. As a rule of thumb: 
+        /// The periapsis should be greater than the radius of the body 
+        /// (and its atmosphere), but the lower the better.
         /// </summary>
         /// <param name="body">The body to eject from</param>
         /// <param name="UT">Univeral time when SOI should be left</param>
         /// <param name="peR">Desired altitude of periapsis of the ejection orbit</param>
+        /// <param name="normal">Desired orbit normal (will not be an exact match most likely)</param>
         /// <param name="exitVelocity">Exit velocity relative to the body (i.e. inside the SOI)</param>
-        public static void IdealEjection(IBody body, double UT, double peR, Vector3d exitVelocity) {
+        public static IOrbit IdealEjection(IBody body, double UT, double peR, Vector3d normal, Vector3d exitVelocity) {
             // Implicitly have the specific energy of the ejection orbit
             double exitEnergy = 0.5 * exitVelocity.sqrMagnitude - body.GravParameter / body.SOIRadius;
             // Magnitude of velocity at periapsis of ejection orbit
@@ -23,17 +24,12 @@ namespace kOSMainframe.Orbital {
 
             // Create a more or less arbitrary plane of reference where exitVelocity points to sampleX
             Vector3d sampleX = exitVelocity.normalized;
-            Vector3d sampleY = Vector3d.Exclude(sampleX, Vector3d.up);
-            if(sampleY.sqrMagnitude < 1e-5) {
-                sampleY = Vector3d.Exclude(sampleX, Vector3d.right).normalized;
-            } else {
-                sampleY = sampleY.normalized;
-            }
-            Vector3d sampleZ = Vector3d.Cross(sampleX, sampleY).normalized;
+            Vector3d sampleZ = Vector3d.Exclude(sampleX, normal).normalized;
+            Vector3d sampleY = Vector3d.Cross(sampleZ, sampleX).normalized;
 
-            // Create a sample orbit in the plane perpendicular to sampleY
+            // Create a sample orbit in the plane perpendicular to sampleZ
             Vector3d sampleStartPos = sampleX * peR;
-            Vector3d sampleStartVelocity = sampleZ * peVelocity;
+            Vector3d sampleStartVelocity = sampleY * peVelocity;
             IOrbit sampleOrbit = body.CreateOrbit(sampleStartPos, sampleStartVelocity, 0);
             // Now we get the true anomaly of the exit point
             double exitTA = sampleOrbit.TrueAnomalyAtRadius(body.SOIRadius) ;
@@ -43,18 +39,20 @@ namespace kOSMainframe.Orbital {
             Vector3d sampleExitVelocity = sampleOrbit.SwappedOrbitalVelocityAtUT(dT);
 
             // By choice of the reference plane neither exitVelocity nor sampleExitVelocity
-            // should have an y-component. So we just have to turn everything
+            // should have an z-component. So we just have to turn everything
             // around sampleY so that sampleExitVelocity points to sampleX as well.
-            double angle = -Math.Atan2(Vector3d.Dot(sampleZ, sampleExitVelocity), Vector3d.Dot(sampleX, sampleExitVelocity));
+            double angle = -Math.Atan2(Vector3d.Dot(sampleY, sampleExitVelocity), Vector3d.Dot(sampleX, sampleExitVelocity));
 
             // This will now be the real starting position and velocity
-            Vector3d startPos = peR * (Math.Cos(angle) * sampleX + Math.Sin(angle) * sampleZ);
-            Vector3d startVel = peVelocity * (Math.Cos(angle) * sampleZ - Math.Sin(angle) * sampleX);
+            Vector3d startPos = peR * (Math.Cos(angle) * sampleX + Math.Sin(angle) * sampleY);
+            Vector3d startVel = peVelocity * (Math.Cos(angle) * sampleY - Math.Sin(angle) * sampleX);
 
             IOrbit ejectionOrbit = body.CreateOrbit(startPos, startVel, UT - dT);
 
             Logging.Debug($"Ejection: exitTA={exitTA} dT={dT} exitV={exitVelocity} ejectV={ejectionOrbit.SwappedOrbitalVelocityAtUT(UT)}");
             Logging.DumpOrbit("Ejection orbit", ejectionOrbit);
+
+            return ejectionOrbit;
         }
     }
 }
