4c7136fe87e71f23654702ae0138cbbefeb1533b
Bodo Junglas <junglas@objectcode.de>
2019-05-19 10:07:25 +0200

    Refactor

--- kOS-Mainframe/Orbital/Helper.cs
+++ kOS-Mainframe/Orbital/Helper.cs
@@ -4,6 +4,23 @@ using kOS.Suffixed;
 
 namespace kOSMainframe.Orbital {
     public static class Helper {
+        /// <summary>
+        /// Create a celestrial frame from two vectors.
+        /// First will become X, second will become Z (ensuring its perpendicular to X.
+        /// </summary>
+        /// <returns>The frame.</returns>
+        /// <param name="x">The desired X direction</param>
+        /// <param name="z">The desired Z direction</param>
+        public static Planetarium.CelestialFrame CreateFrame(Vector3d x, Vector3d z) {
+            var frame = new Planetarium.CelestialFrame();
+
+            frame.X = x.normalized;
+            frame.Z = Vector3d.Exclude(frame.X, z).normalized;
+            frame.Y = Vector3d.Cross(frame.Z, frame.X).normalized;
+
+            return frame;
+        }
+
         public static Orbit OrbitFromStateVectors(Vector3d pos, Vector3d vel, CelestialBody body, double UT) {
             Orbit ret = new Orbit();
             ret.UpdateFromStateVectors(Orbit.Swizzle(pos - body.position), Orbit.Swizzle(vel), body, UT);
--- kOS-Mainframe/Orbital/OrbitEjection.cs
+++ kOS-Mainframe/Orbital/OrbitEjection.cs
@@ -4,6 +4,28 @@ using kOSMainframe.Numerics;
 namespace kOSMainframe.Orbital {
     public static class OrbitEjection {
         /// <summary>
+        /// Create a sample ejection orbit from a given start radius to a
+        /// desiged exit velocity.
+        /// This is more of an internal helper to estimate the time it takes
+        /// to leave the SOI as well as the angle between starting velocity
+        /// and exit velocity.
+        /// </summary>
+        /// <returns>The ejection.</returns>
+        /// <param name="body">The body to eject from</param>
+        /// <param name="frame">The celestrial frame to use</param>
+        /// <param name="peR">Desired altitude of periapsis of the ejection orbit</param>
+        /// <param name="exitVelocity">Magnitude of the SOI exit velocity.</param>
+        /// <param name="peVelocity">Required velocity at the periapsis.</param>
+        public static IOrbit SampleEjection(IBody body, Planetarium.CelestialFrame frame, double peR, double exitVelocity, out double peVelocity) {
+            // Implicitly have the specific energy of the ejection orbit
+            double exitEnergy = 0.5 * exitVelocity * exitVelocity - body.GravParameter / body.SOIRadius;
+            // Magnitude of velocity at periapsis of ejection orbit
+            peVelocity = Math.Sqrt(2 * (exitEnergy + body.GravParameter / peR));
+
+            return body.CreateOrbit(frame.X * peR, frame.Y * peVelocity, 0);
+        }
+
+        /// <summary>
         /// Calculates the "ideal" ejection orbit for a given exit velocity at
         /// a given time. 
         /// To get a unique result the periapsis of the ejection orbit and a
@@ -17,20 +39,12 @@ namespace kOSMainframe.Orbital {
         /// <param name="normal">Desired orbit normal (will not be an exact match most likely)</param>
         /// <param name="exitVelocity">Exit velocity relative to the body (i.e. inside the SOI)</param>
         public static IOrbit IdealEjection(IBody body, double UT, double peR, Vector3d normal, Vector3d exitVelocity) {
-            // Implicitly have the specific energy of the ejection orbit
-            double exitEnergy = 0.5 * exitVelocity.sqrMagnitude - body.GravParameter / body.SOIRadius;
-            // Magnitude of velocity at periapsis of ejection orbit
-            double peVelocity = Math.Sqrt(2 * (exitEnergy + body.GravParameter / peR));
-
-            // Create a more or less arbitrary plane of reference where exitVelocity points to sampleX
-            Vector3d sampleX = exitVelocity.normalized;
-            Vector3d sampleZ = Vector3d.Exclude(sampleX, normal).normalized;
-            Vector3d sampleY = Vector3d.Cross(sampleZ, sampleX).normalized;
+            // Create a more or less arbitrary frame of reference where exitVelocity points to sampleX
+            Planetarium.CelestialFrame frame = Helper.CreateFrame(exitVelocity, normal);
 
             // Create a sample orbit in the plane perpendicular to sampleZ
-            Vector3d sampleStartPos = sampleX * peR;
-            Vector3d sampleStartVelocity = sampleY * peVelocity;
-            IOrbit sampleOrbit = body.CreateOrbit(sampleStartPos, sampleStartVelocity, 0);
+            double peVelocity;
+            IOrbit sampleOrbit = SampleEjection(body, frame, peR, exitVelocity.magnitude, out peVelocity);
             // Now we get the true anomaly of the exit point
             double exitTA = sampleOrbit.TrueAnomalyAtRadius(body.SOIRadius) ;
             // ... the time it takes ot get from periapsis to exit point
@@ -41,11 +55,11 @@ namespace kOSMainframe.Orbital {
             // By choice of the reference plane neither exitVelocity nor sampleExitVelocity
             // should have an z-component. So we just have to turn everything
             // around sampleY so that sampleExitVelocity points to sampleX as well.
-            double angle = -Math.Atan2(Vector3d.Dot(sampleY, sampleExitVelocity), Vector3d.Dot(sampleX, sampleExitVelocity));
+            double angle = -Math.Atan2(Vector3d.Dot(frame.Y, sampleExitVelocity), Vector3d.Dot(frame.X, sampleExitVelocity));
 
             // This will now be the real starting position and velocity
-            Vector3d startPos = peR * (Math.Cos(angle) * sampleX + Math.Sin(angle) * sampleY);
-            Vector3d startVel = peVelocity * (Math.Cos(angle) * sampleY - Math.Sin(angle) * sampleX);
+            Vector3d startPos = peR * (Math.Cos(angle) * frame.X + Math.Sin(angle) * frame.Y);
+            Vector3d startVel = peVelocity * (Math.Cos(angle) * frame.Y - Math.Sin(angle) * frame.X);
 
             IOrbit ejectionOrbit = body.CreateOrbit(startPos, startVel, UT - dT);
 
