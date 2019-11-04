e440224ac4a888a851015c67aca8a0705a03b1d7
Bodo Junglas <junglas@objectcode.de>
2019-11-04 15:58:10 +0100

    Fix framework version

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 msbuild /Property:Configuration=Release
 
-mono packages/NUnit.ConsoleRunner.3.9.0/tools/nunit3-console.exe kOS-Mainframe-Test/bin/Release/kOS-Mainframe-Test.dll
+mono packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe kOS-Mainframe-Test/bin/Release/kOS-Mainframe-Test.dll
